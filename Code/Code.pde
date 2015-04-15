38a1b2cc7ff1a4f30eb6e4e1bfab8c762f270313
RobinGeers <robin.geers@student.howest.be>
2015-04-15 14:49:24 +0200

    Aantal keer botsen + volgend object

--- Code/Code.pde
+++ Code/Code.pde
@@ -7,20 +7,30 @@ int az = 0;
 boolean timerStarted = false;
 ControlP5 cp5;
 int firstTime = 0;
+int firstTimeAfbeelding = 0;
 PImage[] arrOnderdelen = new PImage[3];
 boolean countDownEnded = false;
+boolean userCollision = false;
+int userCollisionCounter = 0;
+float randomAfbeelding;
+int reset = 0;
 
 void setup() {
 	size(600,600);
 	background(255);
 	noStroke();
 	smooth();
-
+	textFont(createFont("Helvetica-Nueu", 15));
 	// Constructor. This = parent
 	cp5 = new ControlP5(this);
 
-	// Button
-	cp5.addButton("startButton").setValue(0).setPosition(100,100).setSize(200,19);
+	// Button 'Start Training'
+	cp5.addButton("startButton").setValue(0).setPosition(50,50).setSize(100,19);
+	cp5.getController("startButton").setCaptionLabel("Start training");
+
+	//
+	
+	//cp5.getController("buttonOpnieuw").hide();
 
 	firstTime = 1;
 }
@@ -31,9 +41,10 @@ toonCountDown();
 
 if (countDownEnded) {
 
-if(firstTimeAfbeelding ==0 ){
-toonRandomAfbeelding();
-}
+	if(firstTimeAfbeelding == 0) {
+		toonRandomAfbeelding();
+		toonTekstInstructie();
+	}
 }
 
 }
@@ -45,10 +56,14 @@ void startButton() {
 	background(255);
 
 	timerStarted = true;
-	
+
+	// Verberg de 'Start training knop'
+	cp5.getController("startButton").hide();	
 }
 
+
 void toonCountDown() {
+
   // Tijd die gepasseerd is
   int passedTime = millis() - savedTime;
 
@@ -87,7 +102,6 @@ void toonCountDown() {
 
     az=1;
 	}
-
 }
 
 void toonAfbeelding(int getal) {
@@ -95,13 +109,72 @@ void toonAfbeelding(int getal) {
 	image(image3, 300, 300);
 }
 
-int firstTimeAfbeelding = 0;
 void toonRandomAfbeelding() {
 	background(255);
 	for (int i = 1; i < arrOnderdelen.length+1; i++) {
 		arrOnderdelen[i-1] = loadImage("Object" + i + ".png");
 	}
-	float randomAfbeelding = random(0, 3);
-	image(arrOnderdelen[int(randomAfbeelding)], 100, 100);
+	randomAfbeelding = random(0, 3);
+	image(arrOnderdelen[int(randomAfbeelding)], 200, 200, 200, 200);
 	firstTimeAfbeelding = 1;
+}
+
+void toonTekstInstructie() {
+	  fill(#000000);
+	  textSize(20);
+	  text("Neem dit object uit het lichaam van Dr. Bibber.", 50, 100);
+}
+
+// TODO: Sluit Makey Makey aan om te testen
+void keyPressed() {
+	if (key == CODED) {
+    	if (keyCode == UP) { // Bots
+	      userCollisionCounter++;
+	  	  background(#e74c3c);
+	  	  image(arrOnderdelen[int(randomAfbeelding)], 200, 200, 200, 200); // Toon opnieuw het getoonde object
+	  	  toonTekstGebotst();
+	  	  toonTekstInstructie();
+		}
+		else if (keyCode == RIGHT) { // Object ligt op veilige plaats
+		  reset = 0; // Zorgt ervoor dat buttonOpnieuw niet iedere keer wordt uitgevoerd als hij wordt aangemaakt
+		  toonButtonOpnieuw();
+		  toonTekstGewonnen();
+		}
+	}
+}
+
+void toonTekstGebotst() {
+	  fill(#000000);
+	  textSize(20);
+	  text("Aantal keren gebotst: " + str(userCollisionCounter), 50, 150);
+}
+
+void toonTekstGewonnen() {
+	  fill(#000000);
+	  background(#2ecc71);
+	  textSize(20);
+	  text("Proficiat u hebt het object! U bent " + userCollisionCounter + " keer gebotst.", 50, 150);
+}
+
+void toonButtonOpnieuw() {
+	cp5.addButton("buttonOpnieuw").setValue(0).setPosition(50,50).setSize(100, 20);
+	cp5.getController("buttonOpnieuw").setCaptionLabel("Volgend object");
+	// http://www.sojamo.de/libraries/controlP5/reference/controlP5/Controller.html ALLE getController properties!
+}
+
+void buttonOpnieuw() {
+	// Zorgt ervoor dat de knop code niet wordt uitgevoerd bij het initialiseren
+	if (firstTime == 0) return;
+	resetAlles();
+	reset = 1;
+}
+
+void resetAlles() {
+	// Zorgt ervoor dat de knop code niet wordt uitgevoerd bij het initialiseren
+	if(reset==0) return;
+	background(255);
+	toonRandomAfbeelding();
+	toonTekstInstructie();
+	userCollisionCounter = 0;
+	
 }
--- Code/build-tmp/source/Code.java
+++ Code/build-tmp/source/Code.java
@@ -25,20 +25,30 @@ int az = 0;
 boolean timerStarted = false;
 ControlP5 cp5;
 int firstTime = 0;
+int firstTimeAfbeelding = 0;
 PImage[] arrOnderdelen = new PImage[3];
 boolean countDownEnded = false;
+boolean userCollision = false;
+int userCollisionCounter = 0;
+float randomAfbeelding;
+int reset = 0;
 
 public void setup() {
 	size(600,600);
 	background(255);
 	noStroke();
 	smooth();
-
+	textFont(createFont("Helvetica-Nueu", 15));
 	// Constructor. This = parent
 	cp5 = new ControlP5(this);
 
-	// Button
-	cp5.addButton("startButton").setValue(0).setPosition(100,100).setSize(200,19);
+	// Button 'Start Training'
+	cp5.addButton("startButton").setValue(0).setPosition(50,50).setSize(100,19);
+	cp5.getController("startButton").setCaptionLabel("Start training");
+
+	//
+	
+	//cp5.getController("buttonOpnieuw").hide();
 
 	firstTime = 1;
 }
@@ -49,12 +59,11 @@ toonCountDown();
 
 if (countDownEnded) {
 
-if(firstTimeAfbeelding ==0 ){
-toonRandomAfbeelding();
+	if(firstTimeAfbeelding == 0) {
+		toonRandomAfbeelding();
+		toonTekstInstructie();
+	}
 }
-}
-
-
 
 }
 
@@ -65,10 +74,14 @@ public void startButton() {
 	background(255);
 
 	timerStarted = true;
-	
+
+	// Verberg de 'Start training knop'
+	cp5.getController("startButton").hide();	
 }
 
+
 public void toonCountDown() {
+
   // Tijd die gepasseerd is
   int passedTime = millis() - savedTime;
 
@@ -107,7 +120,6 @@ public void toonCountDown() {
 
     az=1;
 	}
-
 }
 
 public void toonAfbeelding(int getal) {
@@ -115,15 +127,74 @@ public void toonAfbeelding(int getal) {
 	image(image3, 300, 300);
 }
 
-int firstTimeAfbeelding = 0;
 public void toonRandomAfbeelding() {
 	background(255);
 	for (int i = 1; i < arrOnderdelen.length+1; i++) {
 		arrOnderdelen[i-1] = loadImage("Object" + i + ".png");
 	}
-	float randomAfbeelding = random(0, 3);
-	image(arrOnderdelen[PApplet.parseInt(randomAfbeelding)], 100, 100);
+	randomAfbeelding = random(0, 3);
+	image(arrOnderdelen[PApplet.parseInt(randomAfbeelding)], 200, 200, 200, 200);
 	firstTimeAfbeelding = 1;
+}
+
+public void toonTekstInstructie() {
+	  fill(0xff000000);
+	  textSize(20);
+	  text("Neem dit object uit het lichaam van Dr. Bibber.", 50, 100);
+}
+
+// TODO: Sluit Makey Makey aan om te testen
+public void keyPressed() {
+	if (key == CODED) {
+    	if (keyCode == UP) { // Bots
+	      userCollisionCounter++;
+	  	  background(0xffe74c3c);
+	  	  image(arrOnderdelen[PApplet.parseInt(randomAfbeelding)], 200, 200, 200, 200); // Toon opnieuw het getoonde object
+	  	  toonTekstGebotst();
+	  	  toonTekstInstructie();
+		}
+		else if (keyCode == RIGHT) { // Object ligt op veilige plaats
+		  reset = 0; // Zorgt ervoor dat buttonOpnieuw niet iedere keer wordt uitgevoerd als hij wordt aangemaakt
+		  toonButtonOpnieuw();
+		  toonTekstGewonnen();
+		}
+	}
+}
+
+public void toonTekstGebotst() {
+	  fill(0xff000000);
+	  textSize(20);
+	  text("Aantal keren gebotst: " + str(userCollisionCounter), 50, 150);
+}
+
+public void toonTekstGewonnen() {
+	  fill(0xff000000);
+	  background(0xff2ecc71);
+	  textSize(20);
+	  text("Proficiat u hebt het object! U bent " + userCollisionCounter + " keer gebotst.", 50, 150);
+}
+
+public void toonButtonOpnieuw() {
+	cp5.addButton("buttonOpnieuw").setValue(0).setPosition(50,50).setSize(100, 20);
+	cp5.getController("buttonOpnieuw").setCaptionLabel("Volgend object");
+	// http://www.sojamo.de/libraries/controlP5/reference/controlP5/Controller.html ALLE getController properties!
+}
+
+public void buttonOpnieuw() {
+	// Zorgt ervoor dat de knop code niet wordt uitgevoerd bij het initialiseren
+	if (firstTime == 0) return;
+	resetAlles();
+	reset = 1;
+}
+
+public void resetAlles() {
+	// Zorgt ervoor dat de knop code niet wordt uitgevoerd bij het initialiseren
+	if(reset==0) return;
+	background(255);
+	toonRandomAfbeelding();
+	toonTekstInstructie();
+	userCollisionCounter = 0;
+	
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Code" };
